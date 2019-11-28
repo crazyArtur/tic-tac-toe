f1a58e8b53b8f4d4f4f99ebc51ff417f7045ac68
crazyArtur <artur.dbsz@gmail.com>
2019-11-28 16:35:46 +1100

    Updated handleClick and render methods

--- src/index.js
+++ src/index.js
@@ -50,12 +50,13 @@ function Square(props) {
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
 
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]){
@@ -66,7 +67,7 @@ function Square(props) {
         history: history.concat([{
           squares: squares,
         }]),
-        stepNumber: 0,
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
@@ -80,7 +81,7 @@ function Square(props) {
 
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[this.state.stepNumber];
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
